6b53e4af7bc9701af3918e07180ad7dcfab32afc
rukeeo1 <rukeeojigbo@gmail.com>
2022-03-26 18:50:32 +0100

    chore: set up redux and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import Instagram from './Components/Instagram';
+import { BrowserRouter as Router, Switch, Route,Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import Instagram from "./Components/Instagram";
+import { store } from "./redux/store";
+import Homepage from "./Components/ProfileDetails/ProfileDetails";
 
 function App() {
   return (
-    <div className='App'>
-      <Instagram />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Routes>
+
+          {/* <Instagram /> */}
+          <Route path="/" element={<Homepage />} />
+          </Routes>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
