85dc5d45a28584330f293fed8fc7e26fdb316ac4
rukeeo1 <rukeeojigbo@gmail.com>
2022-04-03 10:36:30 +0100

    fix sign up button

--- src/Components/SignUp/SignupBtn.js
+++ src/Components/SignUp/SignupBtn.js
@@ -1,6 +1,7 @@
-const SignupBtn = ({ className, title, onSubmit = () => {} }) => {
+const SignupBtn = ({ className, title, onSubmit = () => {}, type }) => {
+  console.log(type,'RO: type')
   return (
-    <button className={className} onSubmit={onSubmit}>
+    <button className={className} onClick={onSubmit} type={type}>
       {title}
     </button>
   );
