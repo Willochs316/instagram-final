352de06654a47ce433ec7f5812cef50373fcac3f
Rukee Ojigbo <rukeeojigbo@gmail.com>
2022-04-11 13:53:10 +0100

    added changes

--- src/Components/LoginForm/LoginButton.js
+++ src/Components/LoginForm/LoginButton.js
@@ -1,5 +1,10 @@
-const LoginButton = ({ className, title }) => {
-  return <button className={className}>{title}</button>;
+const LoginButton = ({ className, title, onSubmit = () => {}, type }) => {
+  console.log(type, 'RO: type');
+  return (
+    <button className={className} onClick={onSubmit} type={type}>
+      {title}
+    </button>
+  );
 };
 
 export default LoginButton;
