a2962b6c18838316988817d597c9dbd8e8156f36
Rukee Ojigbo <rukeeojigbo@gmail.com>
2022-04-11 04:39:03 +0100

    chenges made

--- src/Components/NavBar/NavBarImages.js
+++ src/Components/NavBar/NavBarImages.js
@@ -3,24 +3,28 @@ export default [
   {
     id: 0,
     image:
-      'https://cdn-icons.flaticon.com/png/512/3161/premium/3161837.png?token=exp=1647274014~hmac=17b7bb4ad17b4a730beb9df78aa4c59f',
+      'https://cdn2.iconfinder.com/data/icons/instagram-17/32/13-fullfeed-512.png',
   },
   {
     id: 1,
     image:
-      'https://cdn-icons.flaticon.com/png/512/2099/premium/2099144.png?token=exp=1647271266~hmac=9f85d50b04076ee06b3cfce615d29280',
+      'https://cdn1.iconfinder.com/data/icons/instagram-feature-outline/32/icon_instagram-21-512.png',
   },
   {
     id: 2,
     image:
-      'https://cdn-icons.flaticon.com/png/512/3024/premium/3024593.png?token=exp=1647273380~hmac=73e6e9fe2e92536a1cddc5a2fd918082',
+      'https://cdn0.iconfinder.com/data/icons/instagram-ui-1/24/Instagram-UI_send-512.png',
   },
   {
     id: 3,
     image:
       'https://images.unsplash.com/photo-1545912193-31ce2f0e9a08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTZ8Nlh4TjA4RWJNNnd8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60',
   },
-  { id: 4, image: 'https://cdn-icons-png.flaticon.com/512/1077/1077035.png' },
+  {
+    id: 4,
+    image:
+      'https://cdn0.iconfinder.com/data/icons/instagram-ui-1/24/Instagram-UI_like-512.png',
+  },
   {
     id: 5,
     image:
